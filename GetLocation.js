7cce49d716e0efe4d09a8f6a0a4cfe1cf156a8af
shachamt <shachamt@post.bgu.ac.il>
2022-05-15 02:17:49 +0300

    shacham

--- GetLocation.js
+++ GetLocation.js
@@ -1,17 +1,37 @@
 function GetLocation() {
     if (navigator.geolocation) {
-    console.log("in get location");
-    navigator.geolocation.getCurrentPosition(showPosition);
+        console.log("in get location");
+        navigator.geolocation.getCurrentPosition(showPosition);
     } else {
-    document.getElementById("p").innerHTML="Geolocation is not supported by this browser.";
-    }}
-
-
-    function showPosition(position) {
-        var x = document.getElementById("p");
-        var y = document.getElementById("button");
-        x.innerHTML = "Your In <br>" + "Latitude: " + position.coords.latitude +
-        "<br>Longitude: " + position.coords.longitude;
-        y.innerHTML = "try me again";
-        console.log(position);
+        document.getElementById("p").innerHTML = "Geolocation is not supported by this browser.";
     }
+}
+
+
+function showPosition(position) {
+    var x = document.getElementById("p");
+    var y = document.getElementById("button");
+    x.innerHTML = "Your In <br>" + "Latitude: " + position.coords.latitude +
+        "<br>Longitude: " + position.coords.longitude;
+    y.innerHTML = "try me again";
+    console.log(position);
+}
+
+function GetLocationMani() {
+    if (navigator.geolocation) {
+        console.log("in get location");
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        document.getElementById("p1").innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+
+function showPosition(position) {
+    var x = document.getElementById("p1");
+    var y = document.getElementById("buttonLocationMani");
+    console.log(position);
+    document.getElementById("Latitude").value =position.coords.latitude;
+    document.getElementById("Longitude").value =position.coords.longitude;
+
+}
