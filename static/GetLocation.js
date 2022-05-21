6d9693232572511c1071b2beb759948623585aa8
shachamt <shachamt@post.bgu.ac.il>
2022-05-21 15:30:26 +0300

    shacham

--- static/GetLocation.js
+++ static/GetLocation.js
@@ -31,24 +31,74 @@ function showPosition(position) {
     var x = document.getElementById("p1");
     var y = document.getElementById("buttonLocationMani");
     console.log(position);
-    document.getElementById("Latitude").value =position.coords.latitude;
-    document.getElementById("Longitude").value =position.coords.longitude;
+    document.getElementById("Latitude").value = position.coords.latitude;
+    document.getElementById("Longitude").value = position.coords.longitude;
 
 }
 
-function sandMani(){
+function sandMani() {
     alert("Your details have been sent to the manicurist and she will contact you in the next 12 hours");
 }
-function updateMyProfile(){
+function updateMyProfile() {
     alert("Your details were updated");
 }
 
-// להשתמש בזה לטובת ההגבלה של המחירון בדף עריכת הפרופיל העסקי - כרגע לא עובד
-function inputRequired (){  
-    const service= document.getElementsByClassName("service");
-    const price= document.getElementsByClassName("price");
 
-    if(service===""){
-        price.setAttribute("required", "");
+function showPassword() {
+    var pass = document.getElementById("password");
+    var eye1 = document.getElementById("hide1");
+    var eye2 = document.getElementById("hide2");
+
+
+    if (pass.type === 'password') {
+        pass.type = "text";
+        eye1.style.display = "inline";
+        eye2.style.display = "none";
     }
+
+    else {
+        pass.type = "password";
+        eye1.style.display = "none";
+        eye2.style.display = "inline";
+    }
+
 }
+
+
+    const lastName = document.getElementById("lastName")
+    const firstName = document.getElementById("firstName")
+    const password = document.getElementById("password")
+    const formMani = document.getElementById("SignUpManicuristForm")
+    const errorMani = document.getElementById("errorMani")
+    const email = document.getElementById("email")
+    const phone = document.getElementById("telephone")
+    console.log(phone)
+
+    formMani.addEventListener('submit', (e) => {
+        let messages = []
+
+        if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))) {
+        messages.push ('Email is not valid')
+        }
+
+        if(firstName.value.length <2) {
+            messages.push ('First name must be at least 2 characters')
+        }
+
+        if(lastName.value.length <2) {
+            messages.push ('Last name must be at least 2 characters')
+        }
+
+        if(password.value.length <=6) {
+            messages.push ('Password must be loger than 6 characters')
+        }
+
+
+        if (messages.length>0) {
+            e.preventDefault()
+            errorMani.innerText = messages.join (', ')
+        }
+
+    })
+
+    
---
+++ static/validation.js
@@ -0,0 +1,56 @@
+function showPassword() {
+    var pass = document.getElementById("password");
+    var eye1 = document.getElementById("hide1");
+    var eye2 = document.getElementById("hide2");
+
+
+    if (pass.type === 'password') {
+        pass.type = "text";
+        eye1.style.display = "inline";
+        eye2.style.display = "none";
+    }
+
+    else {
+        pass.type = "password";
+        eye1.style.display = "none";
+        eye2.style.display = "inline";
+    }
+
+}
+
+
+const lastName = document.getElementById("lastName")
+const firstName = document.getElementById("firstName")
+const password = document.getElementById("password")
+const formCustomer = document.getElementById("SignUpCustomerForm")
+const errorMani = document.getElementById("errorCustomer")
+const email = document.getElementById("email1")
+const phone = document.getElementById("telephone")
+
+
+formCustomer.addEventListener('submit', (e) => {
+    let messages = []
+
+    if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value))) {
+        messages.push('Email is not valid')
+    }
+
+    if (firstName.value.length < 2) {
+        messages.push('First name must be at least 2 characters')
+    }
+
+    if (lastName.value.length < 2) {
+        messages.push('Last name must be at least 2 characters')
+    }
+
+    if (password.value.length <= 6) {
+        messages.push('Password must be loger than 6 characters')
+    }
+
+
+    if (messages.length > 0) {
+        e.preventDefault()
+        errorMani.innerText = messages.join(', ')
+    }
+
+})
